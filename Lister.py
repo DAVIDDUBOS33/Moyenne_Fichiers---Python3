0e165478d378d1c2dd00ad08b2aafb5216c48503
DAVIDDUBOS33 <46395867+DAVIDDUBOS33@users.noreply.github.com>
2019-01-07 06:17:36 +0100

    Add files via upload

--- Lister.py
+++ Lister.py
@@ -2,7 +2,7 @@ import os
 
 TOTO = os.path.expandvars('$HOME')
 
-def Moyenne_Fichiers(toto, n=0, total=0):
+def Moyenne_Fichiers(toto, n=0, total=0, n_erreur=0):
     """
     La fonction os.walk permet de lister récursivement tous les fichiers et les dossiers à partir d’un point dans l’arborescence.
     :param n: nombre de fichiers
@@ -11,12 +11,15 @@ def Moyenne_Fichiers(toto, n=0, total=0):
     """
     for dossier, sous_dossiers, fichiers in os.walk(toto):
         for fichier in fichiers:
-            try:  # erreur de chemin sur un fichier dans repertoire google ????
+            try:  # erreur de chemin sur un fichier dans repertoire google ???? : FileNotFoundError
                 # print(os.path.join(dossier, fichier))
                 n += 1
                 total += os.stat(os.path.join(dossier, fichier)).st_size  # taille d'un fichier
             except:
-                n = n
+                #print(os.path.join(dossier, fichier))
+                n_erreur +=1
+    if n_erreur >0 :
+        print("'FileNotFoundError: [Errno 2] No such file or directory:' trouvé %s fois" %n_erreur)
     return total // n
 
 print("****  %s octets est la taille moyenne d'un fichier " %Moyenne_Fichiers(TOTO), end = "")
